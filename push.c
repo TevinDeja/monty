242890ab87ae112a794c8318a25340fb11cb01dd
Tevin Deja <tevinowena@gmail.com>
2023-12-13 23:27:06 -0800

    push, pall

---
+++ push.c
@@ -0,0 +1,44 @@
+#include "monty.h"
+
+/**
+ * f_push - Function that adds node to the stack
+ * @head: stack head
+ * @counter:counter number
+ *
+ * Return: 0
+*/
+
+void f_push(stack_t **head, unsigned int counter)
+{
+	int n, m = 0, lag = 0;
+
+	if (bus.arg)
+	{
+		if (bus.arg[0] == '-')
+			m++;
+		for (; bus.arg[m] != '\0'; m++)
+		{
+			if (bus.arg[m] > 57 || bus.arg[m] < 48)
+				lag = 1;
+		}
+		if (lag == 1)
+		{ fprintf(stderr, "L%d: usage: push integer\n", counter);
+			fclose(bus.file);
+			free(bus.content);
+			free_stack(*head);
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{ fprintf(stderr, "L%d: usage: push integer\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	n = atoi(bus.arg);
+	if (bus.lifi == 0)
+		addnode(head, n);
+	else
+		addqueue(head, n);
+}
